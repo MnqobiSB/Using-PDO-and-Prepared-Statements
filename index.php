872a66570b50577a7ed1bce99fa03b444744ed7d
MnqobiSB <amandlamm1@gmail.com>
2021-10-25 11:30:48 +0200

    fetch data with PDO method and display in a tablr

--- index.php
+++ index.php
@@ -6,4 +6,30 @@
     //Display Errors
     echo $e->getMessage();
   }
+
+  /* SELECT DATA */
+  //Create Query
+  $sth = $dbh->query('SELECT * FROM employees');
+
+  //Set Fetch Mode - Array/Objects/Num
+  $sth->setFetchMode(PDO::FETCH_OBJ);
 ?>
+
+<table width="500" cellpadding=5 cellspacing=5 border=1>
+  <tr>
+    <th>ID#</th>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Dept</th>
+    <th>Email</th>
+  </tr>
+  <?php while($row = $sth->fetch()) : ?>
+  <tr>
+    <td><?php echo $row->id; ?></td>
+    <td><?php echo $row->first_name; ?></td>
+    <td><?php echo $row->last_name; ?></td>
+    <td><?php echo $row->department; ?></td>
+    <td><?php echo $row->email; ?></td>
+  </tr>
+  <?php endwhile; ?>
+</table>
